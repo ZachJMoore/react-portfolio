26881dfda178166610cb6979ea2f714b34b683b1
ZachMoore <Get3Moore@gmail.com>
2018-06-28 02:31:33 -0400

    Add basic work project item

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import code from "../images/code.jpg"
+import smartKilnImg from "../images/smart-kiln.jpg"
 
 class Work extends Component {
   render() {
@@ -8,9 +9,22 @@ class Work extends Component {
         <div src={code} className="work-header-image">
             <h2>Work</h2>
         </div>
+        <div className="work-projects">
+            <ProjectItem imgSrc={smartKilnImg} title="Smart Kiln" description="Creating a computer controlled kiln thats accessible from anywhere in the world."/>
+        </div>
       </div>
     );
   }
+}
+
+let ProjectItem = (props)=>{
+    return (
+        <div className="work-project-item">
+            <div className="work-project-image" style={{backgroundImage: `url(${props.imgSrc})`}}/>
+            <span className="work-project-title">{props.title}</span>
+            <span className="work-project-description">{props.description}</span>
+        </div>
+    )
 }
 
 export default Work;
