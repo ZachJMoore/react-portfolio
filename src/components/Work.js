528c8be4d4ba889c7181dc625d0bf24caecb409c
ZachMoore <Get3Moore@gmail.com>
2018-06-28 21:56:38 -0400

    Add new project item

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import code from "../images/code.jpg"
-import smartKilnImg from "../images/smart-kiln.jpg"
+import smartKilnImg from "../images/smart-kiln.jpg";
+import redCeramicBowl from "../images/red-ceramic-bowl.jpg"
 
 class Work extends Component {
   render() {
     return (
       <div className="work-container">
-        <div src={code} className="work-header-image">
-            <h2>Work</h2>
-        </div>
+        <div src={code} className="work-header-image"><span>Work</span></div>
         <div className="work-projects">
             <ProjectItem imgSrc={smartKilnImg} title="Smart Kiln" description="Creating a computer controlled kiln thats accessible from anywhere in the world."/>
+            <ProjectItem imgSrc={redCeramicBowl} title="Crystalline" description="Growing Zinc Silicate crystals at 2225°F"/>
         </div>
       </div>
     );
@@ -20,7 +20,7 @@ class Work extends Component {
 let ProjectItem = (props)=>{
     return (
         <div className="work-project-item">
-            <div className="work-project-image" style={{backgroundImage: `url(${props.imgSrc})`}}/>
+            <div className="work-project-image" style={{backgroundImage: `url(${props.imgSrc})`}}></div>
             <span className="work-project-title">{props.title}</span>
             <span className="work-project-description">{props.description}</span>
         </div>
