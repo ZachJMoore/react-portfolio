7bab5dde0eafd7d6a840221485db15d402e2ac6c
ZachMoore <Get3Moore@gmail.com>
2018-07-02 11:58:59 -0400

    Add new images

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,17 +1,56 @@
 import React, { Component } from 'react';
 import code from "../images/code.jpg"
-import smartKilnImg from "../images/smart-kiln.jpg";
+import raspberryPi from "../images/device.jpg";
 import redCeramicBowl from "../images/red-ceramic-bowl.jpg"
+import marbledCeramicPlate from "../images/marbled-ceramic-plate.jpg"
+import moldedCup from "../images/molded-cup.jpg"
+
+
+let ceramicProjects = [
+  {
+    imgSrc: redCeramicBowl,
+    title: "Crystalline",
+    description: "Growing Zinc Silicate crystals at 2225°F",
+    link: "crystalline-ceramics"
+  },
+  {
+    imgSrc: marbledCeramicPlate,
+    title: "Marbled Ceramics",
+    description: "Visualizing the flow of black and white"
+  },
+  {
+    imgSrc: moldedCup,
+    title: "3D Print Molding",
+    description: "Making precision molds from digital models"
+  }
+]
+
+let softwareProjects = [
+  {
+    imgSrc: raspberryPi,
+    title: "Smart Kiln",
+    description: "Creating a computer controlled kiln thats accessible from anywhere in the world.",
+    link: "smart-kiln"
+  }
+]
+
 
 class Work extends Component {
   render() {
     return (
       <div className="work-container">
         <div src={code} className="work-header-image"><span>Work</span></div>
+
+        {/* <h4 className="work-projects-title">Software</h4> */}
         <div className="work-projects">
-            <ProjectItem imgSrc={smartKilnImg} title="Smart Kiln" description="Creating a computer controlled kiln thats accessible from anywhere in the world."/>
-            <ProjectItem imgSrc={redCeramicBowl} title="Crystalline" description="Growing Zinc Silicate crystals at 2225°F"/>
+          {softwareProjects.map(project => <ProjectItem imgSrc={project.imgSrc} title={project.title} description={project.description}/>)}
         </div>
+
+        {/* <h4 className="work-projects-title">Ceramics</h4> */}
+        <div className="work-projects">
+          {ceramicProjects.map(project => <ProjectItem imgSrc={project.imgSrc} title={project.title} description={project.description}/>)}
+        </div>
+
       </div>
     );
   }
